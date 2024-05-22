df3f9cdf52af583fd3ecc7b14e0133ce636e3a48
kzkio114 <kzkio114@gmail.com>
2024-05-22 17:11:10 +0900

    ボタンの遷移を修正 #97 #99 #16 #18

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,9 +1,11 @@
 class ButtonsController < ApplicationController
-
   def enter_app
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.append("response_area", partial: "buttons/enter_app_response")
+        render turbo_stream: [
+          turbo_stream.append("response_area", partial: "buttons/enter_app_response"),
+          turbo_stream.replace("enter_app_button", partial: "buttons/enter_app_button")
+        ]
       end
     end
   end
@@ -11,7 +13,10 @@ class ButtonsController < ApplicationController
   def login
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.append("response_area", partial: "buttons/login_response")
+        render turbo_stream: [
+          turbo_stream.append("response_area", partial: "buttons/login_response"),
+          turbo_stream.replace("login_button", partial: "buttons/login_button")
+        ]
       end
     end
   end
