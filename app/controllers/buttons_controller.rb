ff388ef3df434548ad835a3a0498d9d3a63a8c8e
kzkio114 <kzkio114@gmail.com>
2024-05-22 21:56:04 +0900

    アプリの説明表示完了 #97 close #16

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -3,6 +3,7 @@ class ButtonsController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
+          turbo_stream.replace("response_area", partial: "buttons/enter_app_response"),
           turbo_stream.append("response_area", partial: "buttons/enter_app_response"),
           turbo_stream.replace("enter_app_button", partial: "buttons/enter_app_button")
         ]
@@ -14,6 +15,7 @@ class ButtonsController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
+          turbo_stream.replace("response_area", partial: "buttons/login_response"),
           turbo_stream.append("response_area", partial: "buttons/login_response"),
           turbo_stream.replace("login_button", partial: "buttons/without_login_button")
         ]
