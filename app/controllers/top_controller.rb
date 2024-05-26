fee5e04741c4f1a745638e8666573565ba26e3cf
kzkio114 <kzkio114@gmail.com>
2024-05-26 13:25:59 +0900

    .envの確認用記載の削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,4 @@
 class TopController < ApplicationController
   def index
-    Rails.logger.info "GOOGLE_CLIENT_SECRET: #{ENV['GOOGLE_CLIENT_SECRET']}"
-    Rails.logger.info "GOOGLE_CLIENT_ID: #{ENV['GOOGLE_CLIENT_ID']}"
   end
-
 end
