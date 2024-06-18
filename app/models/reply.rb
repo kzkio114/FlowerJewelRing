a314507f2720c21c8a1b41aa9d70266e9e8c04d1
kzkio114 <kzkio114@gmail.com>
2024-06-18 15:16:17 +0900

    投稿の時のエラー表示と削除位置の編集

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,5 +2,5 @@ class Reply < ApplicationRecord
   belongs_to :consultation
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: { message: "を入力してください" }, format: { with: /\A(?!\p{Space}*\z).*\S/, message: "は空白文字だけで投稿できません" }, length: { maximum: 5000 }
 end
