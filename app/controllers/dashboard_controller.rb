680465d325dca395934f6fe264bf90e210c38cad
kzkio114 <kzkio114@gmail.com>
2024-06-21 18:21:51 +0900

    メッセージとギフト

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   def index
     @group_chats = GroupChat.all
     @user = current_user
-    replier_ids = current_user.consultations.joins(:replies).pluck('replies.user_id').uniq
-    @received_gifts_from_repliers = current_user.received_gifts.where(giver_id: replier_ids)
+    @unread_gifts_count = current_user.calculate_unread_gifts_count
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2, :discord, :twitter, :github, :line]
 
-
-
-  #has_many :gift_users
-  #has_many :received_gifts, through: :gift_users, source: :gift  # 更新された関連付け
   has_many :admin_users
   has_many :organizations, through: :user_organizations
   has_many :sent_chats, class_name: 'Chat', foreign_key: 'sender_id'
@@ -24,32 +20,29 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-
-   # 新しいメソッドを追加
-   def received_gifts_from_repliers
+  # 新しいメソッドを追加
+  def received_gifts_from_repliers
     replier_ids = consultations.joins(:replies).pluck('replies.user_id').uniq
     received_gifts.where(giver_id: replier_ids)
   end
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first_or_initialize
-  
-    user.name = data['name']
-    user.password = Devise.friendly_token[0, 20] if user.new_record?
-    unless user.save
-      Rails.logger.info("ユーザー保存失敗: #{user.errors.full_messages.to_sentence}")
-      return nil
-    end
-    user
-  end
-
-
-         # ユーザーが持つ相談から、未読の返信数を返すメソッド
+  # ユーザーが持つ相談から、未読の返信数を返すメソッド
   def unread_replies_count
     Reply.joins(:consultation)
          .where(consultations: { user_id: id })
          .where(read: false) # read は返信が読まれたかどうかを示す属性
          .count
+  end
+
+  # 未読のギフト数を計算するメソッド
+  def calculate_unread_gifts_count
+    received_gifts_from_repliers.sum do |gift|
+      unread_histories_count = gift.gift_histories.where(read: false).count
+      sender_message_count = gift.sender_message.present? ? 1 : 0
+
+      Rails.logger.info "Gift ID: #{gift.id}, Unread Histories: #{unread_histories_count}, Sender Message Count: #{sender_message_count}"
+
+      unread_histories_count + sender_message_count
+    end
   end
 end
