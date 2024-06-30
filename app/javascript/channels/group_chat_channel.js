73c87d16c3e2f5df624026b9e9ffd8689c78fc3d
kzkio114 <kzkio114@gmail.com>
2024-06-30 23:25:52 +0900

    js修正等

---
+++ app/javascript/channels/group_chat_channel.js
@@ -0,0 +1,62 @@
+import consumer from "./consumer";
+
+document.addEventListener('turbo:load', () => {
+  const chatElement = document.getElementById('group_chat');
+
+  if (chatElement) {
+    const groupChatId = chatElement.dataset.groupChatId;
+    const groupChatChannel = consumer.subscriptions.create(
+      { channel: "GroupChatChannel", group_chat_id: groupChatId },
+      {
+        connected() {
+          console.log("Connected to the GroupChatChannel");
+        },
+        disconnected() {
+          console.log("Disconnected from the GroupChatChannel");
+        },
+        received(data) {
+          const messages = document.getElementById('messages');
+          if (data.action === 'create' && messages) {
+            messages.insertAdjacentHTML('beforeend', data.message_html);
+          } else if (data.action === 'destroy' && data.message_id) {
+            const messageElement = document.getElementById(`message_${data.message_id}`);
+            if (messageElement) {
+              messageElement.remove();
+            }
+          }
+        },
+        speak(message) {
+          this.perform('speak', { message: message, group_chat_id: groupChatId });
+        },
+        deleteMessage(messageId) {
+          this.perform('delete_message', { message_id: messageId });
+        }
+      }
+    );
+
+    const input = document.getElementById('chat_message_input');
+    const button = document.querySelector('input[type="submit"]');
+
+    if (input && button) {
+      const sendMessage = () => {
+        const message = input.value;
+        if (message.trim() !== '') {
+          groupChatChannel.speak(message);
+          input.value = '';
+        }
+      };
+
+      input.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+          sendMessage();
+          event.preventDefault();
+        }
+      });
+
+      button.addEventListener('click', (event) => {
+        sendMessage();
+        event.preventDefault();
+      });
+    }
+  }
+});
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,3 +1,3 @@
 // Import all the channels to be used by Action Cable
 import "./chat_channel"
-import "./group_chat"
+import "./group_chat_channel"
