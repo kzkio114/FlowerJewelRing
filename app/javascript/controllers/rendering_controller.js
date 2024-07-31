724a05c3915dd9f650f939e5b13efc60fee4af1f
kzkio114 <kzkio114@gmail.com>
2024-07-31 22:52:41 +0900

    ホバーで文字を表記

--- app/javascript/controllers/rendering_controller.js
+++ app/javascript/controllers/rendering_controller.js
@@ -1,11 +1,10 @@
-// rendering_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = [
     "details", "toggleDetails", "extraContent",
     "news", "worries", "search", "chat", "thanks",
-    "showNews", "showWorries", "showSearch", "showChat", "showThanks"
+    "newsImage", "worriesImage", "searchImage", "chatImage", "thanksImage"
   ];
 
   connect() {
@@ -15,6 +14,14 @@ export default class extends Controller {
       showSearch: this.searchTarget,
       showChat: this.chatTarget,
       showThanks: this.thanksTarget
+    };
+
+    this.imageMapping = {
+      showNews: this.newsImageTarget,
+      showWorries: this.worriesImageTarget,
+      showSearch: this.searchImageTarget,
+      showChat: this.chatImageTarget,
+      showThanks: this.thanksImageTarget
     };
   }
 
@@ -35,17 +42,70 @@ export default class extends Controller {
   }
 
   showContent(event) {
-    // 全てのセクションを非表示にする
+    // 全てのセクションと画像を非表示にする
     Object.values(this.buttonMapping).forEach(section => {
       section.classList.add("is-hidden");
     });
 
-    // クリックされたボタンに対応するセクションを表示
+    Object.values(this.imageMapping).forEach(image => {
+      image.classList.add("is-hidden");
+    });
+
+    // クリックされたボタンに対応するセクションと画像を表示
     const targetId = event.currentTarget.id;
     const targetElement = this.buttonMapping[targetId];
-    if (targetElement) {
+    const targetImage = this.imageMapping[targetId];
+
+    if (targetElement && targetImage) {
       targetElement.classList.remove("is-hidden");
+      targetImage.classList.remove("is-hidden");
       this.extraContentTarget.classList.remove("is-hidden");
+
+      // クリック時のテキストに変更
+      this.activateButtonState(event.currentTarget, "click");
+    }
+  }
+
+  hoverContent(event) {
+    const button = event.currentTarget;
+    const hoverText = button.getAttribute("data-hover-text");
+
+    // 元のテキストを保存
+    if (!button.hasAttribute("data-original-text")) {
+      button.setAttribute("data-original-text", button.textContent);
+    }
+
+    // ホバーテキストを表示
+    if (hoverText) {
+      button.textContent = hoverText;
+    }
+  }
+
+  resetContent(event) {
+    const button = event.currentTarget;
+    // ホバーが解除されたときに、元のテキストに戻す
+    if (!button.hasAttribute("data-clicked")) {
+      const originalText = button.getAttribute("data-original-text");
+      if (originalText) {
+        button.textContent = originalText;
+      }
+    }
+  }
+
+  activateButtonState(button, type) {
+    if (button) {
+      if (type === "click") {
+        button.setAttribute("data-clicked", "true"); // クリック状態を記録
+        const clickText = button.getAttribute("data-click-text");
+        if (clickText) {
+          button.textContent = clickText;
+        }
+      } else {
+        const hoverText = button.getAttribute("data-hover-text");
+        if (hoverText) {
+          button.textContent = hoverText;
+        }
+      }
     }
   }
 }
