dd113c62c56e9a70eac03b7430f0a73d5ce3c531
kzkio114 <kzkio114@gmail.com>
2024-09-05 18:43:38 +0900

    データベースの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_02_232152) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_094223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_232152) do
     t.datetime "updated_at", null: false
     t.boolean "completed"
     t.string "desired_reply_tone"
-    t.string "display_choice"
+    t.string "display_choice", default: "name"
     t.index ["category_id"], name: "index_consultations_on_category_id"
     t.index ["user_id"], name: "index_consultations_on_user_id"
   end
