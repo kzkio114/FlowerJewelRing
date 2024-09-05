2536c648cda2cbb9d2c945e5223614f28dd91a2d
kzkio114 <kzkio114@gmail.com>
2024-09-05 17:29:42 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -118,40 +118,63 @@
 #   )
 # end
 
-# 宝石カテゴリを作成
-jewel_category = GiftCategory.find_or_create_by!(name: '宝石') do |category|
-  category.description = '美しい宝石のギフト'
+# # 宝石カテゴリを作成
+# jewel_category = GiftCategory.find_or_create_by!(name: '宝石') do |category|
+#   category.description = '美しい宝石のギフト'
+# end
+
+# # 宝石の種類と説明を配列に格納
+# jewels = [
+#   ["オニキス", "夫婦の幸福 厄除け 秘密"],
+#   ["アクアマリン", "勇敢 聡明 沈着"],
+#   ["琥珀", "活性 長寿 繁栄"],
+#   ["スピネル", "愛情 幸福 ポジティブ"],
+#   ["ルビー", "純愛 仁愛 情熱的な愛"],
+#   ["エメラルド", "愛 癒し 聡明"],
+#   ["サファイア", "愛情 誠実 徳望"],
+#   ["アメジスト", "心の平和 真実の愛 誠実"],
+#   ["オパール", "純真無垢 幸運 忍耐"],
+#   ["ダイヤモンド", "純愛 純潔 清浄無垢"],
+#   ["真珠", "健康 純粋 長寿"],
+#   ["トパーズ", "成功 希望 誠実"],
+#   ["ペリドット", "太陽の象徴 夫婦和合 厄除け"],
+#   ["シトリン", "繁栄 幸運 成功"],
+#   ["タンザナイト", "神秘 冷静 誇り高い"],
+#   ["ガーネット", "真実 友愛 忠実"]
+# ]
+
+# # 各宝石に対応するギフトテンプレートを作成
+# jewels.each_with_index do |(jewel, message), i|
+#   GiftTemplate.find_or_create_by!(
+#     gift_category: jewel_category,
+#     name: jewel,
+#     description: message, # 宝石の意味をdescriptionに保存
+#     color: 'blue',
+#     image_url: "gem#{i+1}.webp" # 画像のURLは適宜調整してください
+#   )
+# end
+
+# puts "宝石カテゴリとギフトが作成されました。"
+
+# 全ユーザーにランダムで10個のギフトをテンプレートからあげる
+User.find_each do |user|
+  # ランダムに10個のギフトテンプレートを取得
+  gift_templates = GiftTemplate.order("RANDOM()").limit(10)
+
+  # ギフトテンプレートからギフトを作成してユーザーに割り当てる
+  gift_templates.each do |template|
+    Gift.create!(
+      gift_template_id: template.id,
+      gift_category: template.gift_category,
+      item_name: template.name,
+      description: template.description,
+      image_url: template.image_url,
+      color: template.color,
+      receiver_id: user.id,  # ユーザーにギフトを割り当てる
+      giver_id: nil,         # 贈り主は指定しない（nil）
+      sent_at: nil           # 送られた日時も未設定
+    )
+  end
 end
 
-# 宝石の種類と説明を配列に格納
-jewels = [
-  ["オニキス", "夫婦の幸福 厄除け 秘密"],
-  ["アクアマリン", "勇敢 聡明 沈着"],
-  ["琥珀", "活性 長寿 繁栄"],
-  ["スピネル", "愛情 幸福 ポジティブ"],
-  ["ルビー", "純愛 仁愛 情熱的な愛"],
-  ["エメラルド", "愛 癒し 聡明"],
-  ["サファイア", "愛情 誠実 徳望"],
-  ["アメジスト", "心の平和 真実の愛 誠実"],
-  ["オパール", "純真無垢 幸運 忍耐"],
-  ["ダイヤモンド", "純愛 純潔 清浄無垢"],
-  ["真珠", "健康 純粋 長寿"],
-  ["トパーズ", "成功 希望 誠実"],
-  ["ペリドット", "太陽の象徴 夫婦和合 厄除け"],
-  ["シトリン", "繁栄 幸運 成功"],
-  ["タンザナイト", "神秘 冷静 誇り高い"],
-  ["ガーネット", "真実 友愛 忠実"]
-]
-
-# 各宝石に対応するギフトテンプレートを作成
-jewels.each_with_index do |(jewel, message), i|
-  GiftTemplate.find_or_create_by!(
-    gift_category: jewel_category,
-    name: jewel,
-    description: message, # 宝石の意味をdescriptionに保存
-    color: 'blue',
-    image_url: "gem#{i+1}.webp" # 画像のURLは適宜調整してください
-  )
-end
-
-puts "宝石カテゴリとギフトが作成されました。"
+puts "全ユーザーにギフトがランダムで10個ずつ割り当てられました。"
