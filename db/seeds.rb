5ba52fb8b0c5d3cc32f1b5cbc2165ed5b706a6d5
kzkio114 <kzkio114@gmail.com>
2024-09-05 17:06:13 +0900

    全体修正完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 #   category.description = '美しい植物のギフト'
 # end
 
-# # 各日付の花と花言葉を配列に格納
+# # 各日付の花と花言葉or宝石言葉を配列に格納
 # flowers = [
 #   ["フクジュソウ", "幸せを招く"],
 #   ["ハルサザンカ", "困難に勝つ"],
@@ -32,12 +32,12 @@
 #   ["ロウバイ", "温かみ"]
 # ]
 
-# # 各日付の花と花言葉に対応するギフトを作成
+# # 各日付の花と花言葉or宝石言葉に対応するギフトを作成
 # flowers.each_with_index do |(flower, message), i|
 #   Gift.find_or_create_by!(
 #     gift_category: plant_category,
 #     item_name: flower,
-#     description: message, # 花言葉をdescriptionに保存
+#     description: message, # 花言葉or宝石言葉をdescriptionに保存
 #     color: 'green',
 #     image_url: "#{i+10}.webp", # 画像のURLは適宜調整してください
 #     sender_message: "" # sender_messageは空にしておく
@@ -57,66 +57,66 @@
 
 # puts 'カテゴリとコンサルテーションのデータが生成されました。'
 
-# admin
-# ユーザーを見つけます
+# # admin
+# # ユーザーを見つけます
 
- user = User.find_by(email: 'kzkio114@gmail.com')
-
-
-# # 既存のAdminUserレコードを見つけるか、新しいレコードを作成します
- admin_user = AdminUser.find_or_create_by(user: user, organization_id: 1)
-
-# # admin_roleをsuper_adminに設定します
- admin_user.update(admin_role: :super_admin)
+#  user = User.find_by(email: 'kzkio114@gmail.com')
 
 
+# # # 既存のAdminUserレコードを見つけるか、新しいレコードを作成します
+#  admin_user = AdminUser.find_or_create_by(user: user, organization_id: 1)
+
+# # # admin_roleをsuper_adminに設定します
+#  admin_user.update(admin_role: :super_admin)
 
 
-# ギフトカテゴリを作成
 
-# 既存のギフトカテゴリを使用
-plant_category = GiftCategory.find_or_create_by!(name: '植物') do |category|
-  category.description = '美しい植物のギフト'
-end
 
-# 各日付の花と花言葉を配列に格納
-flowers = [
-  ["フクジュソウ", "幸せを招く"],
-  ["ハルサザンカ", "困難に勝つ"],
-  ["ブルースター", "幸せな愛"],
-  ["ラッパスイセン", "再生"],
-  ["マンリョウ", "予言"],
-  ["ナワシロイチゴ", "恩恵"],
-  ["ブルーデイジー", "恵まれて"],
-  ["グズマニア", "完璧"],
-  ["スイートピー", "永遠の喜び"],
-  ["リカステ", "快活"],
-  ["オウバイ", "高貴"],
-  ["チューリップ（黄）", "正直"],
-  ["ピグミーランタン", "印象"],
-  ["キンギョソウ", "欲望"],
-  ["サクラソウ", "忠実"],
-  ["ハナシノブ", "野生美"],
-  ["クリスマスローズ", "大切な人"],
-  ["スプレーギク", "寛大"],
-  ["サクシフラガ", "自信"],
-  ["ネコヤナギ", "素直"],
-  ["ウメ（紅）", "忠実"],
-  ["バイモ", "飾らない心"],
-  ["ミズバショウ", "決心"],
-  ["ロウバイ", "温かみ"]
-]
+# # ギフトカテゴリを作成
 
-# 各日付の花と花言葉に対応するギフトテンプレートを作成
-flowers.each_with_index do |(flower, message), i|
-  GiftTemplate.find_or_create_by!(
-    gift_category: plant_category,
-    name: flower,
-    description: message, # 花言葉をdescriptionに保存
-    color: 'green',
-    image_url: "#{i+10}.webp" # 画像のURLは適宜調整してください
-  )
-end
+# # 既存のギフトカテゴリを使用
+# plant_category = GiftCategory.find_or_create_by!(name: '植物') do |category|
+#   category.description = '美しい植物のギフト'
+# end
+
+# # 各日付の花と花言葉or宝石言葉を配列に格納
+# flowers = [
+#   ["フクジュソウ", "幸せを招く"],
+#   ["ハルサザンカ", "困難に勝つ"],
+#   ["ブルースター", "幸せな愛"],
+#   ["ラッパスイセン", "再生"],
+#   ["マンリョウ", "予言"],
+#   ["ナワシロイチゴ", "恩恵"],
+#   ["ブルーデイジー", "恵まれて"],
+#   ["グズマニア", "完璧"],
+#   ["スイートピー", "永遠の喜び"],
+#   ["リカステ", "快活"],
+#   ["オウバイ", "高貴"],
+#   ["チューリップ（黄）", "正直"],
+#   ["ピグミーランタン", "印象"],
+#   ["キンギョソウ", "欲望"],
+#   ["サクラソウ", "忠実"],
+#   ["ハナシノブ", "野生美"],
+#   ["クリスマスローズ", "大切な人"],
+#   ["スプレーギク", "寛大"],
+#   ["サクシフラガ", "自信"],
+#   ["ネコヤナギ", "素直"],
+#   ["ウメ（紅）", "忠実"],
+#   ["バイモ", "飾らない心"],
+#   ["ミズバショウ", "決心"],
+#   ["ロウバイ", "温かみ"]
+# ]
+
+# # 各日付の花と花言葉or宝石言葉に対応するギフトテンプレートを作成
+# flowers.each_with_index do |(flower, message), i|
+#   GiftTemplate.find_or_create_by!(
+#     gift_category: plant_category,
+#     name: flower,
+#     description: message, # 花言葉or宝石言葉をdescriptionに保存
+#     color: 'green',
+#     image_url: "#{i+10}.webp" # 画像のURLは適宜調整してください
+#   )
+# end
 
 # 宝石カテゴリを作成
 jewel_category = GiftCategory.find_or_create_by!(name: '宝石') do |category|
@@ -136,7 +136,7 @@ jewels = [
   ["オパール", "純真無垢 幸運 忍耐"],
   ["ダイヤモンド", "純愛 純潔 清浄無垢"],
   ["真珠", "健康 純粋 長寿"],
-  ["トパーズ", "成功、希望、誠実"],
+  ["トパーズ", "成功 希望 誠実"],
   ["ペリドット", "太陽の象徴 夫婦和合 厄除け"],
   ["シトリン", "繁栄 幸運 成功"],
   ["タンザナイト", "神秘 冷静 誇り高い"],
@@ -150,7 +150,7 @@ jewels.each_with_index do |(jewel, message), i|
     name: jewel,
     description: message, # 宝石の意味をdescriptionに保存
     color: 'blue',
-    image_url: "gem#{i+1}_jewel.webp" # 画像のURLは適宜調整してください
+    image_url: "gem#{i+1}.webp" # 画像のURLは適宜調整してください
   )
 end
 
