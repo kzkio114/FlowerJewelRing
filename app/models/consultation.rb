2a2348004cd943e73214bb25e0a1384f4d49d1aa
kzkio114 <kzkio114@gmail.com>
2024-09-05 12:07:48 +0900

    説明の追加と修正等

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -6,7 +6,7 @@ class Consultation < ApplicationRecord
       title: title,
       content: content,
       category_name: category.name,
-      user_name: user.name
+      user_name: anonymized_name # 匿名対応の名前を使う
     }
   end
 
@@ -18,4 +18,9 @@ class Consultation < ApplicationRecord
   validates :content, presence: { message: "を入力してください" }, format: { with: /\A(?!\p{Space}*\z).*\S/, message: "は空白文字だけで投稿できません" }, length: { maximum: 5000 }
 
   enum desired_reply_tone: { positive: '肯定', neutral: '普通', negative: '否定' }, _suffix: true
+
+  # 匿名かどうかを判定し、匿名の場合は '匿名' を返す
+  def anonymized_name
+    display_choice == 'anonymous' ? '匿名' : user.name
+  end
 end
